89bd6cac63138b58c8d4f40123162bfec1db1b42
Kittiphop22 <156067699+Kittiphop22@users.noreply.github.com>
2024-08-11 12:16:35 +0700

    Add function components
    Add function components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './style.css'
 import Calendar from './class-components';
+import { Header,Content,Footer } from './func-components';
 
 function App(){
   return (
     <>
+    <Header/><Content/><Footer/>
     <h2>DeveloperChina.com</h2>
     <div className='title'>React & React Native</div>
     <p><center><Calendar/></center></p>
